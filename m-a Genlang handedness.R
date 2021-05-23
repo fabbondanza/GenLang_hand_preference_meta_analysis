c4fe913f1561fe260dc6aaf8c402a244b5311bb6
filippo.abbondanza <33876104+fabbondanza@users.noreply.github.com>
2021-05-23 22:52:18 +0100

    Add files via upload

--- m-a Genlang handedness.R
+++ m-a Genlang handedness.R
@@ -7,50 +7,50 @@ library(meta)
 library(metafor)
 
 # Read file #####
-
-# GenLang below ####
-
-# LOR_SLI <- escalc(measure = 'OR',ai = `SLI Left-Handers`, ci = `SLI Total N` - `SLI Left-Handers`, bi = `TD Left-Handers`, di = `TD Total N`-`TD Left-Handers`, data = read_xlsx('genlang_all_cohorts_updated.xlsx', sheet = 'SLI-All'))
-# LOR_SLI <- escalc(measure = 'OR',n1i = `SLI Total N`, n2i = `TD Total N`, x1i = `SLI Left-Handers`, x2i = `TD Left-Handers`, data = read_xlsx('genlang_all_cohorts_updated.xlsx', sheet = 'SLI-All'))
-
 LOR_SLI <- read_xlsx('genlang_all_cohorts_updated.xlsx', sheet = 'SLI-All') %>%
-  filter(Study != 'YORK') %>% 
+  # filter(Study != 'Manchester', Study != 'UKDYS') %>%
   mutate(OR = (`SLI Left-Handers`/(`SLI Total N` - `SLI Left-Handers`))/(`TD Left-Handers`/(`TD Total N`-`TD Left-Handers`)) )
 
 LOR_SLI_Female <- read_xlsx('genlang_all_cohorts_updated.xlsx', sheet = 'SLI-Female') %>%
+  # filter(Study != 'Manchester', Study != 'UKDYS') %>%
   mutate(OR = (`SLI Left-Handers`/(`SLI Total N` - `SLI Left-Handers`))/(`TD Left-Handers`/(`TD Total N`-`TD Left-Handers`)) )
 
 LOR_SLI_Male <- read_xlsx('genlang_all_cohorts_updated.xlsx', sheet = 'SLI-Male') %>%
+  # filter(Study != 'Manchester', Study != 'UKDYS') %>%
   mutate(OR = (`SLI Left-Handers`/(`SLI Total N` - `SLI Left-Handers`))/(`TD Left-Handers`/(`TD Total N`-`TD Left-Handers`)) )
 
 
 LOR_RD <- read_xlsx('genlang_all_cohorts_updated.xlsx', sheet = 'RD-All') %>%
-  filter(Study != 'NTR') %>%
+  filter(Study != 'NTR') %>% 
+  filter(Study != 'York_ALSPAC') %>%
+  # filter(Study != 'Manchester', Study != 'UKDYS') %>%
   mutate(OR = (`RD Left-Handers`/(`RD Total N` - `RD Left-Handers`))/(`TD Left-Handers`/(`TD Total N`-`TD Left-Handers`)) )
 
 LOR_RD_Male <- read_xlsx('genlang_all_cohorts_updated.xlsx', sheet = 'RD-Male') %>%
-  filter(Study != 'NTR') %>%
+  filter(Study != 'NTR') %>% 
+  # filter(Study != 'Manchester', Study != 'UKDYS') %>%
   mutate(OR = (`RD Left-Handers`/(`RD Total N` - `RD Left-Handers`))/(`TD Left-Handers`/(`TD Total N`-`TD Left-Handers`)) )
 
 LOR_RD_Female <- read_xlsx('genlang_all_cohorts_updated.xlsx', sheet = 'RD-Female') %>%
-  filter(Study != 'NTR') %>%
+  # filter(Study != 'Manchester', Study != 'UKDYS') %>%
+  filter(Study != 'NTR') %>% 
   mutate(OR = (`RD Left-Handers`/(`RD Total N` - `RD Left-Handers`))/(`TD Left-Handers`/(`TD Total N`-`TD Left-Handers`)) )
 
 
 LOR_COMBINED <- read_xlsx('genlang_all_cohorts_updated.xlsx', sheet = 'Combined2') %>%
   filter(Study != 'Peters') %>%
-  filter(Study != 'Manchester') %>%
+  # filter(Study != 'Manchester', Study != 'UKDYS') %>%
+  filter(Study != 'York_ALSPAC') %>%
   mutate(OR = (`Combined Left-Handers`/(`Combined Total N` - `Combined Left-Handers`))/(`TD Left-Handers`/(`TD Total N`-`TD Left-Handers`)) )
 
 LOR_COMBINED_Female <- read_xlsx('genlang_all_cohorts_updated.xlsx', sheet = 'Combined-Female2') %>%
   filter(Study != 'Peters') %>%
-  filter(Study != 'Manchester') %>%
+  # filter(Study != 'Manchester', Study != 'UKDYS') %>%
   mutate(OR = (`Combined Left-Handers`/(`Combined Total N` - `Combined Left-Handers`))/(`TD Left-Handers`/(`TD Total N`-`TD Left-Handers`)) )
 
 LOR_COMBINED_Male <- read_xlsx('genlang_all_cohorts_updated.xlsx', sheet = 'Combined-Male2') %>%
-  filter(Study != 'York') %>% 
   filter(Study != 'Peters') %>%
-  filter(Study != 'Manchester') %>%
+  # filter(Study != 'Manchester', Study != 'UKDYS') %>%
   mutate(OR = (`Combined Left-Handers`/(`Combined Total N` - `Combined Left-Handers`))/(`TD Left-Handers`/(`TD Total N`-`TD Left-Handers`)) )
 
 
@@ -70,8 +70,8 @@ meta.LOR_RD <- metabin(`RD Left-Handers`, # Taken from https://bookdown.org/Math
                     # hakn = TRUE, # Not sure how Marietta did it before. ONLY FOR RANDOM EFFECT use if trials have similar sample size
                     prediction = TRUE,
                     warn = TRUE,
-                    label.e = "RD",
-                    label.c = "TD",
+                    label.e = "Cases",
+                    label.c = "Controls",
                     title = "Non-right-handedness RD vs TD",
                     # incr = 0.1, # Only for small sample size, deafault is 0.5 in the example is found is 1
                     sm = "OR")
@@ -91,8 +91,8 @@ meta.LOR_RD_Male <- metabin(`RD Left-Handers`, # Taken from https://bookdown.org
                        # hakn = TRUE, # Not sure how Marietta did it before. ONLY FOR RANDOM EFFECT use if trials have similar sample size
                        prediction = TRUE,
                        warn = TRUE,
-                       label.e = "RD",
-                       label.c = "TD",
+                       label.e = "Cases",
+                       label.c = "Controls",
                        title = "Non-right-handedness RD vs TD",
                        # incr = 0.1, # Only for small sample size, deafault is 0.5 in the example is found is 1
                        sm = "OR")
@@ -112,8 +112,8 @@ meta.LOR_RD_Female <- metabin(`RD Left-Handers`, # Taken from https://bookdown.o
                             # hakn = TRUE, # Not sure how Marietta did it before. ONLY FOR RANDOM EFFECT use if trials have similar sample size
                             prediction = TRUE,
                             warn = TRUE,
-                            label.e = "RD",
-                            label.c = "TD",
+                            label.e = "Cases",
+                            label.c = "Controls",
                             title = "Non-right-handedness RD vs TD",
                             # incr = 0.1, # Only for small sample size, deafault is 0.5 in the example is found is 1
                             sm = "OR")
@@ -134,8 +134,8 @@ meta.LOR_SLI <- metabin(`SLI Left-Handers`, # Taken from https://bookdown.org/Ma
                        # hakn = TRUE, # Not sure how Marietta did it before
                        prediction = TRUE,
                        warn = TRUE,
-                       label.e = "DLD",
-                       label.c = "TD",
+                       label.e = "Cases",
+                       label.c = "Controls",
                        title = "Non-right-handedness DLD vs TD",
                        # incr = 0.1, # Only for small sample size, deafault is 0.5 in the example is found is 1
                        sm = "OR")
@@ -155,8 +155,8 @@ meta.LOR_SLI_Female <- metabin(`SLI Left-Handers`, # Taken from https://bookdown
                         # hakn = TRUE, # Not sure how Marietta did it before
                         prediction = TRUE,
                         warn = TRUE,
-                        label.e = "DLD",
-                        label.c = "TD",
+                        label.e = "Cases",
+                        label.c = "Controls",
                         title = "Non-right-handedness DLD vs TD",
                         # incr = 0.1, # Only for small sample size, deafault is 0.5 in the example is found is 1
                         sm = "OR")
@@ -169,15 +169,15 @@ meta.LOR_SLI_Male <- metabin(`SLI Left-Handers`, # Taken from https://bookdown.o
                                studlab = Study,
                                comb.fixed = F,
                                comb.random = T,
-                             # byvar = Cohort,
-                             print.byvar = T,
+                               # byvar = Cohort,
+                               print.byvar = T,
                                level=0.95,
                                # method.tau = "SJ",
                                # hakn = TRUE, # Not sure how Marietta did it before
                                prediction = TRUE,
                                warn = TRUE,
-                               label.e = "DLD",
-                               label.c = "TD",
+                               label.e = "Cases",
+                               label.c = "Controls",
                                title = "Non-right-handedness DLD vs TD",
                                # incr = 0.1, # Only for small sample size, deafault is 0.5 in the example is found is 1
                                sm = "OR")
@@ -200,7 +200,7 @@ meta.LOR_COMBINED <- metabin(`Combined Left-Handers`, # Taken from https://bookd
                        prediction = TRUE,
                        warn = TRUE,
                        label.e = "Combined",
-                       label.c = "TD",
+                       label.c = "Controls",
                        title = "Non-right-handedness Combined vs TD",
                        # incr = 0.1, # Only for small sample size, deafault is 0.5 in the example is found is 1
                        sm = "OR")
@@ -221,7 +221,7 @@ meta.LOR_COMBINED_Female <- metabin(`Combined Left-Handers`, # Taken from https:
                              prediction = TRUE,
                              warn = TRUE,
                              label.e = "Combined",
-                             label.c = "TD",
+                             label.c = "Controls",
                              title = "Non-right-handedness Combined vs TD",
                              # incr = 0.1, # Only for small sample size, deafault is 0.5 in the example is found is 1
                              sm = "OR")
@@ -233,7 +233,7 @@ meta.LOR_COMBINED_Male <- metabin(`Combined Left-Handers`, # Taken from https://
                              data = LOR_COMBINED_Male,
                              studlab = Study,
                              byvar = Cohort,
-                             # print.byvar = T,
+                             print.byvar = T,
                              comb.fixed = F,
                              comb.random = T,
                              level=0.95,
@@ -242,7 +242,7 @@ meta.LOR_COMBINED_Male <- metabin(`Combined Left-Handers`, # Taken from https://
                              prediction = TRUE,
                              warn = TRUE,
                              label.e = "Combined",
-                             label.c = "TD",
+                             label.c = "Controls",
                              title = "Non-right-handedness Combined vs TD",
                              # incr = 0.1, # Only for small sample size, deafault is 0.5 in the example is found is 1
                              sm = "OR")
@@ -263,36 +263,35 @@ summary(meta.LOR_COMBINED)
 summary(meta.LOR_COMBINED_Female)
 summary(meta.LOR_COMBINED_Male)
 
+
 # print(robumeta::robu(formula = OR ~ Cohort, data = LOR_COMBINED, studynum = study, var.eff.size = variance_100, modelweights = "CORR", small = FALSE))
 
 
 # Forest plot ####
-forest(meta.LOR_RD, comb.fixed = F, comb.random = T, print.zval = T, print.pval.Q = T, print.I2.ci = T,
-       print.tau.ci = T, prediction = F, col.fixed = T, overall = T, smlab = "Left hand Odds Ratio RD", sortvar = OR)
-forest(meta.LOR_RD_Male, comb.fixed = F, comb.random = T, print.zval = T, print.pval.Q = T, print.I2.ci = T,
-       print.tau.ci = T, prediction = F, overall = T, smlab = "Left hand Odds Ratio RD Male", sortvar = OR)
-forest(meta.LOR_RD_Female, comb.fixed = F, comb.random = T, print.zval = T, print.pval.Q = T, print.I2.ci = T,
-       print.tau.ci = T, prediction = F, overall = T, smlab = "Left hand Odds Ratio RD Female", sortvar = OR)
+forest(meta.LOR_RD, comb.fixed = F, comb.random = T, print.pval.Q = T, print.I2.ci = T,
+       print.tau.ci = T, prediction = F, overall = T, smlab = "All", lab.e = 'Reading impairment')
+forest(meta.LOR_RD_Male, comb.fixed = F, comb.random = T, print.pval.Q = T, print.I2.ci = T,
+       print.tau.ci = T, prediction = F, overall = T, lab.e = "Reading impairment", smlab = 'Males')
+forest(meta.LOR_RD_Female, comb.fixed = F, comb.random = T, print.pval.Q = T, print.I2.ci = T,
+       print.tau.ci = T, prediction = F, overall = T, lab.e = "Reading impairment", smlab = 'Females')
 
 
 forest(meta.LOR_SLI, comb.fixed = F, comb.random = T, print.zval = T, print.pval.Q = T, print.I2.ci = T,
-       print.tau.ci = T, prediction = F, overall = T, smlab = "Left hand Odds Ratio DLD", sortvar = OR)
-forest(meta.LOR_SLI_Female, comb.fixed = F, comb.random = T, print.zval = T, print.pval.Q = T, print.I2.ci = T,
-       print.tau.ci = T, prediction = F, overall = T, smlab = "Left hand OR DLD Females", sortvar = OR)
+       print.tau.ci = T, prediction = F, overall = T, smlab = "All", lab.e = "Language impairment")
 forest(meta.LOR_SLI_Male, comb.fixed = F, comb.random = T, print.zval = T, print.pval.Q = T, print.I2.ci = T,
-       print.tau.ci = T, prediction = F, overall = T, smlab = "Left hand OR DLD Males", sortvar = OR)
+       print.tau.ci = T, prediction = F, overall = T, smlab = "Males", lab.e = "Language impairment")
+forest(meta.LOR_SLI_Female, comb.fixed = F, comb.random = T, print.zval = T, print.pval.Q = T, print.I2.ci = T,
+       print.tau.ci = T, prediction = F, overall = T, smlab = "Females",lab.e = "Language impairment")
 
-
-forest(meta.LOR_COMBINED, comb.fixed = F, comb.random = T, print.zval = F, print.pval.Q = T, print.I2.ci = T,
-       prediction = F, overall = T, smlab = "All", sortvar = OR)
-forest(meta.LOR_COMBINED_Male, comb.fixed = F, comb.random = T, print.stat = T, print.pval.Q = T, print.I2.ci = T,
-       print.tau.ci = T, prediction = F, overall = T, smlab = "Males", sortvar = OR)
-forest(meta.LOR_COMBINED_Female, comb.fixed = F, comb.random = T, print.zval = T, print.pval.Q = T, print.I2.ci = T,
-       print.tau.ci = T, prediction = F, overall = T, smlab = "Females", sortvar = OR)
+forest(meta.LOR_COMBINED, comb.fixed = F, comb.random = T, print.pval.Q = T, print.I2.ci = T, lab.e = 'Combined cases',
+       prediction = F, overall = T, smlab = "All", subgroup = T, col.by = 'black')
+forest(meta.LOR_COMBINED_Male, comb.fixed = F, comb.random = T, print.pval.Q = T, print.I2.ci = T, lab.e = 'Combined cases',
+       print.tau.ci = T, prediction = F, overall = T, smlab = "Males", col.by = 'black')
+forest(meta.LOR_COMBINED_Female, comb.fixed = F, comb.random = T, print.zval = T, print.pval.Q = T, print.I2.ci = T, lab.e = 'Combined cases',
+       print.tau.ci = T, prediction = F, overall = T, smlab = "Females", col.by = 'black')
 
 
 # Funnel plots ####
-funnel(meta.LOR_RD, comb.fixed = F,backtransf = F, title = "Funnel plot of Standard Error by Log odds ratio Left Hand RD", studlab = T )
+funnel(meta.LOR_RD, comb.fixed = F,backtransf = F, main = "Funnel plot of Standard Error by Log odds ratio Left Hand RD", studlab = T )
 funnel(meta.LOR_SLI, comb.fixed =F ,backtransf = F, main = "Funnel plot of Standard Error by Log odds ratio Left Hand SLI", studlab = T)
-funnel(meta.LOR_COMBINED, comb.fixed = F,backtransf = F, title = "Funnel plot of Standard Error by Log odds ratio Left Hand SLI", studlab = T, cex.studlab = 0.6, pos.studlab = '3', cex = 0.8)
-
+funnel(meta.LOR_COMBINED, comb.fixed = F,backtransf = F, title = "Funnel plot of Standard Error by Log odds ratio Left Hand SLI", studlab = T, cex.studlab = 0.7)
